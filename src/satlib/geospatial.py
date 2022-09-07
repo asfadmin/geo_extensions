0114add1aa77f278a0b6777ed11b8b6abe9de425
Rohan Weeden <reweeden@alaska.edu>
2022-09-06 16:03:59 -0800

    Add IDL handling to common lib
    Drops the old functionality of merging points that are close together
    since it's not clear why that was needed. If it becomes needed in the
    future we can add it back in *but with a unit test that demonstrates a
    scenario in which it's needed*.

---
+++ src/satlib/geospatial.py
@@ -0,0 +1,62 @@
+import logging
+from typing import List
+
+from shapely.geometry import LineString, Polygon
+from shapely.ops import linemerge, polygonize, unary_union
+
+Point = List[float]
+Bbox = List[Point]
+
+ANTIMERIDIAN = LineString([(180, 90), (180, -90)])
+
+
+def _adjust_lon(lon: float, max_lon: float) -> float:
+    if lon > 180.0:
+        return lon - 360
+    elif lon == 180.0 and max_lon == 180.0:
+        return 179.999
+    elif lon == 180.0:
+        return -179.999
+
+    return lon
+
+
+def _has_closure_point(bbox: Bbox) -> bool:
+    assert len(bbox) >= 2
+
+    return bbox[0] == bbox[-1]
+
+
+def split_bbox_on_idl(bbox: Bbox):
+    """Perform adjustment when the bounding box crosses the 180 longitude line.
+
+    CMR requires the bounding box to be split into two separate bounding bboxes
+    to avoid the polygon being interpreted as wrapping the long way around the
+    Earth.
+    """
+    # Shift into 0-360 range
+    shifted_polygon = Polygon([((360.0 + lon) % 360, lat) for lat, lon in bbox])
+    if not shifted_polygon.intersects(ANTIMERIDIAN):
+        if _has_closure_point(bbox):
+            bbox = bbox[:-1]
+        return [bbox]
+    merged = linemerge([shifted_polygon.boundary, ANTIMERIDIAN])
+    borders = unary_union(merged)
+    polygons = polygonize(borders)
+
+    # Shift back to -180-180 range
+    new_bboxes = []
+    for p in polygons:
+        new_bbox = [[lat, lon] for lon, lat in p.boundary.coords]
+        max_lon = max(lon for lat, lon in new_bbox)
+        new_bbox = [[lat, _adjust_lon(lon, max_lon)] for lat, lon in new_bbox]
+
+        if _has_closure_point(bbox):
+            # CMR does not want closure point
+            new_bboxes.append(new_bbox[:-1])
+        else:
+            logging.debug("IDL crossing splinter is being ignored")
+            # TODO(reweeden): How does this happen? Add a test case
+            raise RuntimeError("Test me!", bbox)
+
+    return new_bboxes
